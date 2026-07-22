c6de47fc56690d052ed4293ecf6732723af2248e
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-07-22 10:40:21 -0500

    Drop x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/triplet";
 
     mangopkgs = {
       url = "github:unmango/pkgs";
